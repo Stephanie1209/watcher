a2077b1bc57dc34eb8251e9d944e81af05354b63
Joao Moura <jg.moura.almeida@gmail.com>
2016-06-28 01:52:19 -0700

    Add open and closed issues counter

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,10 @@
 class RepositoriesController < ApplicationController
-  
   def show
-    @repository = GithubRepository.new params[:name] 
-    @issues = repository_issues 
+    @repository = GithubRepository.new params[:name]
+    @issues = repository_issues
     @pull_requests = repository_pull_requests
+    @open_issues_count = count_open_repository_issues
+    @closed_issues_count = count_closed_repository_issues
   end
 
   private
@@ -12,7 +13,16 @@ class RepositoriesController < ApplicationController
     @repository.issues.map { |issue| Issue.new issue }
   end
 
+  def count_open_repository_issues
+    @issues.keep_if { |issue| issue.state == "open"}.count
+  end
+
+  def count_closed_repository_issues
+    @issues.keep_if { |issue| issue.state == "closed"}.count
+  end
+
   def repository_pull_requests
     @repository.pull_requests.map { |pr| PullRequest.new pr }
   end
 end
+

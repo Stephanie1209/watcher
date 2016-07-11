f45cb8d1d659a2ad658a849a4331f576326fe11d
Joao Moura <jg.moura.almeida@gmail.com>
2016-07-11 15:50:04 -0500

    Small changes to variables in issues_info method

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -13,11 +13,11 @@ module Api
       def issues_info
         @organization = find_github_organization(params[:id])
 
-        @all_issues_and_prs = @client.org_issues(params[:id], { filter: "all", state: "all" }).map { |issue| Issue.new issue }
-        @issues = @all_issues_and_prs.select { |issue| !issue.pull_request? }
-        @issues_count = @issues.count
-        @open_issues_count = @issues.select { |issue| issue.state == "open" }.count
-        @closed_issues_count = @issues.select { |issue| issue.state == "closed" }.count
+        all_issues_and_prs = @client.org_issues(params[:id], { filter: "all", state: "all" }).map { |issue| Issue.new issue }
+        issues = all_issues_and_prs.select { |issue| !issue.pull_request? }
+        @issues_count = issues.count
+        @open_issues_count = issues.select { |issue| issue.state == "open" }.count
+        @closed_issues_count = issues.select { |issue| issue.state == "closed" }.count
       end
     end
   end

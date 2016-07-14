1f9890a4d38f54b0e0723d79745c736cc726bedb
Joao Moura <jg.moura.almeida@gmail.com>
2016-07-14 12:47:10 -0500

    Add specs for all, open, and closed PRs

--- spec/controllers/api/v1/repos_controller_spec.rb
+++ spec/controllers/api/v1/repos_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Api::V1::ReposController, :vcr do
       get :all_issues, id: 'icalialabs', repository_id: 'watcher', format: :json
     end
 
-    it "returns the correct number of open and closed issues in watcher(13)" do
+    it "returns the correct number of open and closed issues in watcher(17)" do
       expect(assigns(:issues).count).to eq(17)
     end
 
@@ -21,8 +21,8 @@ RSpec.describe Api::V1::ReposController, :vcr do
       get :open_issues, id: 'icalialabs', repository_id: 'watcher', format: :json
     end
 
-    it "returns the correct number of open issues in watcher(3)" do
-      expect(assigns(:open_issues).count).to eq(7)
+    it "returns the correct number of open issues in watcher(6)" do
+      expect(assigns(:open_issues).count).to eq(6)
     end
 
     it "should be succesful" do
@@ -35,8 +35,50 @@ RSpec.describe Api::V1::ReposController, :vcr do
       get :closed_issues, id: 'icalialabs', repository_id: 'watcher', format: :json
     end
 
-    it "returns the correct number of closed issues in watcher(10)" do
-      expect(assigns(:closed_issues).count).to eq(10)
+    it "returns the correct number of closed issues in watcher(11)" do
+      expect(assigns(:closed_issues).count).to eq(11)
+    end
+
+    it "should be succesful" do
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET#all_pull_requests", type: :controller do
+    before(:each) do
+      get :all_pull_requests, id: 'icalialabs', repository_id: 'watcher', format: :json
+    end
+
+    it "returns the correct number of open and closed pull requests in watcher(12)" do
+      expect(assigns(:pull_requests).count).to eq(12)
+    end
+
+    it "should be succesful" do
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET#open_pull_requests", type: :controller do
+    before(:each) do
+      get :open_pull_requests, id: 'icalialabs', repository_id: 'watcher', format: :json
+    end
+
+    it "returns the correct number of open pull requests in watcher(1)" do
+      expect(assigns(:open_pull_requests).count).to eq(1)
+    end
+
+    it "should be succesful" do
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET#closed_pull_requests", type: :controller do
+    before(:each) do
+      get :closed_pull_requests, id: 'icalialabs', repository_id: 'watcher', format: :json
+    end
+
+    it "returns the correct number of closed pull requests in watcher(11)" do
+      expect(assigns(:closed_pull_requests).count).to eq(11)
     end
 
     it "should be succesful" do

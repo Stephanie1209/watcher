39308c770b7642358ca4a360c933ee8dc423fb70
Pedro Sordo <pedro.asgarza@gmail.com>
2016-07-15 16:57:07 -0500

    adds organizations model spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,26 +4,20 @@ RSpec.describe Organization, vcr: true do
   before(:each) do
     @client = GithubData.new.client
     organization_data = @client.organization("icalialabs")
-    @organization = Organization.new organization_data
+    @organization = Organization.create(github_name: organization_data["login"],
+                                  description: organization_data["description"],
+                                  avatar: organization_data["avatar_url"])
   end
 
-  it "verifies organization is not empty" do
-    expect(@organization.name).to eql('IcaliaLabs')
+  it 'verifies organizations html_url' do
+    expect(@organization.github_name).to eq('IcaliaLabs')
   end
 
-  it 'verifies public repos count' do
-    expect(@organization.public_repos).to eql(51)
+  it 'verifies organizations description' do
+    expect(@organization.description).to eq('We guide our clients through an agile process, staying ahead of their competition and leveraging opportunities with digital products.')
   end
 
-  it 'verifies public and private repos count' do
-    expect(@organization.total_repos).to eq(70)
-  end
-
-  it 'verifies github html_url' do
-    expect(@organization.github_account).to eq('https://github.com/IcaliaLabs')
-  end
-
-  it 'verifies avatar url' do
+  it 'verifies organizations url' do
     expect(@organization.avatar).to eq("https://avatars.githubusercontent.com/u/2523244?v=3")
   end
 

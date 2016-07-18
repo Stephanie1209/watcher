8d71147eaf162f33579aecdecf06664d2abab1ea
Joao Moura <jg.moura.almeida@gmail.com>
2016-07-18 11:16:16 -0500

    Change repos routes :id to :organization_id, correct controller and specs

--- app/controllers/api/v1/repos_controller.rb
+++ app/controllers/api/v1/repos_controller.rb
@@ -33,16 +33,16 @@ module Api
 
       def branches
         @organization = find_github_organization(params[:id])
-        branches_data = @client.branches("#{params[:id]}/#{params[:repository_id]}")
+        branches_data = @client.branches("#{params[:organization_id]}/#{params[:repository_id]}")
         @branches = branches_data.map { |data| Branch.new data }
       end
 
       private
 
       def search_for_specific_repository
-        @organization = find_github_organization(params[:id])
-        repo = @client.repository("#{params[:id]}/#{params[:repository_id]}")
-        issues = @client.list_issues("#{params[:id]}/#{params[:repository_id]}", issues_query_options)
+        @organization = find_github_organization(params[:organization_id])
+        repo = @client.repository("#{params[:organization_id]}/#{params[:repository_id]}")
+        issues = @client.list_issues("#{params[:organization_id]}/#{params[:repository_id]}", issues_query_options)
         @repository = Repository.new(repo, issues)
       end
 

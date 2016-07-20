58a38ca62a51d9860f044344743016f85a3a31d7
Pedro Sordo <pedro.asgarza@gmail.com>
2016-07-20 11:40:27 -0500

    modifies organization service

--- app/service/organization_service.rb
+++ app/service/organization_service.rb
@@ -5,21 +5,30 @@ class OrganizationService
 
   def initialize organization_name
     ::Octokit.auto_paginate = true
-    client = Octokit::Client.new(access_token: github_token)
+    client = Octokit::Client.new(access_token: ENV["GITHUB_ACCESS_TOKEN"])
+  end
+
+  def set_organization
     @organization = client.organization organization_name
   end
 
   def creates_or_updates_organization
     organization = Organization.find_by_github_name(organization_name) ||
                   Organization.new
-    organization.update_organization_info(@organization)
+    organization.update(github_name: github_name,
+                        description: description,
+                        avatar: avatar)
   end
 
-  def update_organization_info data
-    update(
-      github_name: data["login"],
-      description: data["description"]
-      avatar: data["avatar_url"]
-    )
+  def github_name
+    set_organization["login"]
+  end
+
+  def description
+    set_organization["description"]
+  end
+
+  def avatar
+    set_organization["avatar_url"]
   end
 end

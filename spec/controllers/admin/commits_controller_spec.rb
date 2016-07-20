a810fb32a4b50aaae9845b3b243ab2cbbeec70f5
Joao Moura <jg.moura.almeida@gmail.com>
2016-07-20 11:09:15 -0500

    Changes to specs due to routing and gem changes

--- spec/controllers/admin/commits_controller_spec.rb
+++ spec/controllers/admin/commits_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Admin::CommitsController, type: :controller do
   describe "GET #index" do
     it "renders the :index view" do
-      get :index, :branch_id => 'watcher'
+      get :index, :repository_id => 'watcher', :branch_id => 'master'
       expect(response).to render_template :index
     end
   end

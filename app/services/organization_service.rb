0dd901f1adbc202bbd15843ce8415e22925ec9cf
Pedro Sordo <pedro.asgarza@gmail.com>
2016-07-20 15:54:09 -0500

    renames get_organization method and corrects testing

--- app/services/organization_service.rb
+++ app/services/organization_service.rb
@@ -5,22 +5,21 @@ class OrganizationService
 
   def initialize organization_name
     @github_name = organization_name
-    ::Octokit.auto_paginate = true
     @client = Octokit::Client.new(access_token: ENV["GITHUB_ACCESS_TOKEN"])
   end
 
-  def set_organization
-    @organization = @client.organization @github_name
+  def obtains_organization_data
+    @data = @client.organization @github_name
   end
 
   def creates_or_updates_organization
-    set_organization
-    organization = Organization.find_by_github_name(@organization["login"]) ||
+    obtains_organization_data
+    organization = Organization.find_by_github_name(@data["login"]) ||
                   Organization.new
     organization.update(
-                        github_name: @organization["login"],
-                        description: @organization["description"],
-                        avatar:      @organization["avatar_url"]
+                        github_name: @data["login"],
+                        description: @data["description"],
+                        avatar:      @data["avatar_url"]
                         )
   end
 end
--- spec/services/organization_service_spec.rb
+++ spec/services/organization_service_spec.rb
@@ -1,16 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationService, vcr: true do
-  it "creates a new organization" do
-    service = OrganizationService.new "IcaliaLabs"
-    service.creates_or_updates_organization
-    expect(Organization.last.github_name).to eq("IcaliaLabs")
+  describe "when organization doesnt exist" do
+    it "should create a new organization" do
+      expect  {
+          service = OrganizationService.new "IcaliaLabs"
+          service.creates_or_updates_organization
+          expect(Organization.last.github_name).to eq("IcaliaLabs")
+      }.to change(Organization, :count).by(1)
+    end
   end
 
-  it "updates an organization" do
-    Organization.create(github_name: "IcaliaLabs")
-    service = OrganizationService.new "IcaliaLabs"
-    service.creates_or_updates_organization
-    expect(Organization.last.description).to eq("We guide our clients through an agile process, staying ahead of their competition and leveraging opportunities with digital products.")
+  describe "when organization exists" do
+    it "updates an organization" do
+      expect {
+        FactoryGirl.build(:organization)
+        service = OrganizationService.new "IcaliaLabs"
+        service.creates_or_updates_organization
+        expect(Organization.first.description).to eq("We guide our clients through an agile process, staying ahead of their competition and leveraging opportunities with digital products.")
+      }.to change(Organization, :count).by(1)
+    end
   end
 end

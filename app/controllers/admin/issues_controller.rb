ba26c76246785a3b5863b3d6abe24eec6481394b
Joao Moura <jg.moura.almeida@gmail.com>
2016-07-26 16:45:03 -0500

    Add ajax to all, open, and closed buttons to display issues table

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -1,11 +1,21 @@
 class Admin::IssuesController < AdminController
   def index
     @organization = Organization.find_by_github_name("IcaliaLabs")
-    @repository = @organization.repositories.find_by(name: params[:repository_id])
+    @repository = @organization.repositories.find_by_name(params[:repository_id])
     @issues = @repository.issues.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
     @issue = Issue.find(params[:id])
+  end
+
+  private
+
+  def issues_params
+    params.require(:issue).permit(:status)
   end
 end

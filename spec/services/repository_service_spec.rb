1c107a025c01cd681407959f7b9dcf7084f244ee
Pedro Sordo <pedro.asgarza@gmail.com>
2016-07-27 15:14:37 -0500

    corrects repository service tests

--- spec/services/repository_service_spec.rb
+++ spec/services/repository_service_spec.rb
@@ -5,31 +5,44 @@ RSpec.describe RepositoryService, vcr: true do
     @organization = FactoryGirl.create :icalialabs
   end
 
-  describe "when a repository doesn't exist" do
-    it "should create a new repository" do
-      expect  {
-        service = RepositoryService.new @organization.github_name, "watcher"
-        service.creates_or_updates_repository
-      }.to change(Repository, :count).by(1)
+  describe "creating or updating specific repository" do
+    describe "when a repository doesn't exist" do
+      it "should create a new repository" do
+        expect  {
+          service = RepositoryService.new "watcher"
+          service.creates_or_updates_specific_repository
+        }.to change(Repository, :count).by(1)
+      end
+    end
+
+    describe "when a repository exists" do
+      before :each do
+        @repository = FactoryGirl.create :repository, name: "furatto", organization: @organization
+      end
+
+      it "does not create an organization" do
+        expect {
+          service = RepositoryService.new @repository.name
+          service.creates_or_updates_specific_repository
+        }.to change(Repository, :count).by(0)
+      end
+
+      it "updates the requested repository" do
+        service = RepositoryService.new @repository.name
+        service.creates_or_updates_specific_repository
+        expect(service.repository.id).to eq(@repository.id)
+      end
     end
   end
 
-  describe "when a repository exists" do
-    before :each do
-      @repository = FactoryGirl.create :repository, name: "Furatto", organization: @organization
-    end
-
-    it "does not create an organization" do
-      expect {
-        service = RepositoryService.new @organization.github_name, @repository.name
-        service.creates_or_updates_repository
-      }.to change(Repository, :count).by(0)
-    end
-
-    it "updates the requested repository" do
-      service = RepositoryService.new @organization.github_name, @repository.name
-      service.creates_or_updates_repository
-      expect(service.repository.id).to eq(@repository.id)
+  describe "creating or updating all repositories" do
+    describe "when a repository doesn't exist" do
+      it "should create a new repository" do
+        expect  {
+          service = RepositoryService.new
+          service.creates_or_updates_repositories
+        }.to change(Repository, :count).by(72)
+      end
     end
   end
 end

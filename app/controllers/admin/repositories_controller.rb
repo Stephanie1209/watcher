e5837fe4b8e80d129a46860592ff55eb98f289cc
Pedro Sordo <pedro.asgarza@gmail.com>
2016-07-27 14:57:50 -0500

    refactors repositoryservice and repository controller

--- app/controllers/admin/repositories_controller.rb
+++ app/controllers/admin/repositories_controller.rb
@@ -8,29 +8,14 @@ class Admin::RepositoriesController < AdminController
   end
 
   def update_specific_repository
-    repo = RepositoryService.new(ENV["github_organization"], params[:repo_name])
-    repo.creates_or_updates_repository
+    repo = RepositoryService.new(params[:repo_name])
+    repo.creates_or_updates_specific_repository
     redirect_to :back
   end
 
   def update_repositories
-    github_client
-    repositories = @client.org_repos(ENV["github_organization"],
-                            { query: { per_page: 100, page: 1 } })
-    repositories.each do |repo|
-      creates_or_updates_repository repo
-    end
-    redirect_to admin_repositories_path
-  end
-
-  def creates_or_updates_repository repo
-    organization = Organization.find_by_github_name(ENV["github_organization"])
-    repository = organization.repositories.find_by_name(repo["name"]) || organization.repositories.new
-    repository.update(
-                      name: repo["name"],
-                      stars: repo["stargazers_count"],
-                      forks_count: repo["forks_count"],
-                      started_at: repo["created_at"]
-                      )
+    repo = RepositoryService.new
+    repo.creates_or_updates_repositories
+    redirect_to :back
   end
 end
--- app/services/repository_service.rb
+++ app/services/repository_service.rb
@@ -3,18 +3,22 @@ require 'octokit'
 class RepositoryService
   attr_reader :repository
 
-  def initialize organization_id, repository_id
+  def initialize repository_id = nil
     @repo_id = repository_id
-    ::Octokit.auto_paginate = true
     @client = Octokit::Client.new(access_token: ENV["GITHUB_ACCESS_TOKEN"])
-    @organization = Organization.find_by_github_name(organization_id)
+    @organization = Organization.find_by_github_name(ENV["github_organization"])
   end
 
   def obtains_repository_data
-    @data = @client.repository("#{@organization.github_name}/#{@repo_id}")
+    if @repo_id == nil
+      @data = @client.org_repos(@organization.github_name,
+                              { query: { per_page: 100, page: 1 } })
+    else
+      @data = @client.repository("#{@organization.github_name}/#{@repo_id}")
+    end
   end
 
-  def creates_or_updates_repository
+  def creates_or_updates_specific_repository
     obtains_repository_data
     @repository = @organization.repositories.find_by_name(@data["name"]) || @organization.repositories.new
     @repository.update(
@@ -23,5 +27,19 @@ class RepositoryService
                       forks_count: @data["forks_count"],
                       started_at: @data["created_at"]
                      )
+  end
+
+
+  def creates_or_updates_repositories
+    obtains_repository_data
+    @data.each do |repo|
+      @repository = @organization.repositories.find_by_name(repo["name"]) || @organization.repositories.new
+      @repository.update(
+                        name: repo["name"],
+                        stars: repo["stargazers_count"],
+                        forks_count: repo["forks_count"],
+                        started_at: repo["created_at"]
+                        )
+    end
   end
 end

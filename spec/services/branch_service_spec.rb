0842fca9dca83a022793889afef60426a518b999
Pedro Sordo <pedro.asgarza@gmail.com>
2016-07-29 12:03:31 -0500

    updates spec

--- spec/services/branch_service_spec.rb
+++ spec/services/branch_service_spec.rb
@@ -7,29 +7,41 @@ RSpec.describe BranchService, vcr: true do
     @repository = FactoryGirl.create :repository, name: 'watcher', organization: @organization
   end
 
+  describe "on single branch" do
+    describe "when branch doesnt exist" do
+      it "should create a new branch" do
+        expect  {
+            service = BranchService.new "watcher", "dev"
+            service.creates_or_updates_specific_branch
+            expect(Branch.last.name).to eq("dev")
+        }.to change(Branch, :count).by(1)
+      end
 
-  describe "when branch doesnt exist" do
-    it "should create a new branch" do
-      expect  {
-          service = BranchService.new "watcher", "dev"
-          service.creates_or_updates_branch
-          expect(Branch.last.name).to eq("dev")
-      }.to change(Branch, :count).by(1)
+      it 'verifies branch-repository association' do
+        service = BranchService.new "watcher", "dev"
+        service.creates_or_updates_specific_branch
+        expect(service.branch.repository).to eq(@repository)
+      end
     end
 
-    it 'verifies branch-repository association' do
-      service = BranchService.new "watcher", "dev"
-      service.creates_or_updates_branch
-      expect(service.branch.repository).to eq(@repository)
+    describe "when branch exists" do
+      it "doesnt create a branch" do
+        new_branch = FactoryGirl.create :branch, name: 'dev', repository: @repository
+        service = BranchService.new "watcher", "dev"
+        service.creates_or_updates_specific_branch
+        expect(Branch.count).to eq(1)
+      end
     end
   end
 
-  describe "when branch exists" do
-    it "doesnt create a branch" do
-      new_branch = FactoryGirl.create :branch, name: 'dev', repository: @repository
-      service = BranchService.new "watcher", "dev"
-      service.creates_or_updates_branch
-      expect(Branch.count).to eq(1)
+  describe "on multiple branches" do
+    describe "when branch doesnt exist" do
+      it "should create a new branch" do
+        expect  {
+            service = BranchService.new "watcher"
+            service.creates_or_updates_branches
+        }.to change(Branch, :count).by(5)
+      end
     end
   end
 end

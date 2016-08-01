f6170246ab656ed3c47234f7f9f065a77e2dddf9
Pedro Sordo <pedro.asgarza@gmail.com>
2016-08-01 11:11:32 -0500

    adds commited_at search to commits

--- app/controllers/admin/commits_controller.rb
+++ app/controllers/admin/commits_controller.rb
@@ -3,12 +3,20 @@ class Admin::CommitsController < AdminController
     @commits = []
     @repository = Repository.find(params[:repository_id])
     if params[:branch_id]
-      @commits = @repository.branches.find(params[:branch_id]).commits
-      respond_to do |format|
-        format.js
-      end
+      commits_for params[:branch_id], params[:since], params[:until]
     else
       @commits = @repository.branches.find_by_name("master").commits
+    end
+  end
+
+  def commits_for branch_id, since_date = nil, until_date = nil
+    @commits = @repository.branches.find(branch_id).commits
+    unless (since_date && until_date) == ""
+      @commits = @commits.where(committed_at: since_date..until_date )
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 end

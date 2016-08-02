5fd175a02cb07a266896779f672737d9cd98723b
Pedro Sordo <pedro.asgarza@gmail.com>
2016-08-02 15:02:38 -0500

    removes unused method from admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,8 +2,4 @@ require 'octokit'
 
 class AdminController < ApplicationController
   before_action :authenticate_user!
-
-  def github_client
-    @client = Octokit::Client.new(access_token: ENV["GITHUB_ACCESS_TOKEN"])
-  end
 end

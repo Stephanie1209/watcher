6f9af692ee7f06fe62615ef963754638653068e4
Pedro Sordo <pedro.asgarza@gmail.com>
2016-08-03 11:20:03 -0500

    refactors since-until search for commits

--- app/controllers/admin/commits_controller.rb
+++ app/controllers/admin/commits_controller.rb
@@ -6,13 +6,16 @@ class Admin::CommitsController < AdminController
   end
 
   def commits_for branch_id, since_date = nil, until_date = nil
-    if params[:branch_id]
-      @commits = @repository.branches.find(branch_id).commits
-      unless (since_date && until_date) == ""
-        @commits = @commits.where(committed_at: since_date..until_date )
-      end
-    else
-      @commits = @repository.branches.find_by_name("master").commits
-    end
+    master = @repository.branches.find_by_name("master")
+    @commits = @repository.branches.find(branch_id || master).commits
+    @commits =  if !since_date.blank? && !until_date.blank?
+                  @commits.where(committed_at: since_date..until_date )
+                elsif since_date.blank? && !until_date.blank?
+                  @commits.where("committed_at <= ?", until_date)
+                elsif !since_date.blank? && until_date.blank?
+                  @commits.where("committed_at >= ?", since_date)
+                else
+                  @commits = @repository.branches.find(branch_id).commits
+                end
   end
 end

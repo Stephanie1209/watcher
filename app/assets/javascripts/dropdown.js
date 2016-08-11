9af2833be4664b80000ed3cb7ff3f9d27f52d2c1
Sara de León <saraleon@icalialabs.com>
2016-08-11 17:10:40 -0500

    fixes nav style

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,8 @@
+$(".menu").click(function(){
+		console.log("hola");
+  $(".menu .dropdown").toggle();
+});
+$(".user").click(function(){
+		console.log("hola");
+  $(".user .dropdown").toggle();
+});

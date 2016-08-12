7992643bb71dade664ef1afbadfd3b944875b778
Sara de León <saraleon@icalialabs.com>
2016-08-12 11:17:13 -0500

    fixes repositories name and delates logs in javascript

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,8 +1,6 @@
 $(".menu").click(function(){
-		console.log("hola");
   $(".menu .dropdown").toggle();
 });
 $(".user").click(function(){
-		console.log("hola");
   $(".user .dropdown").toggle();
 });

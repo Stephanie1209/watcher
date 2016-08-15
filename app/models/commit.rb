b6fb873e1dc7988c9a2952d105eaa582be79f943
Joao Moura <jg.moura.almeida@gmail.com>
2016-08-15 11:27:57 -0500

    Change started_at to datetime, order commits by newest

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,5 +1,5 @@
 class Commit < ActiveRecord::Base
   belongs_to :branch
-
+  scope :order_by_newest, -> { order(committed_at: :desc) }
   scope :between_dates, -> (since, to) { where("committed_at >= ? AND committed_at <= ?", since, to) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811175225) do
+ActiveRecord::Schema.define(version: 20160815154528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20160811175225) do
     t.string   "github_number"
     t.string   "github_id"
     t.string   "assignee"
-    t.string   "started_at"
+    t.datetime "started_at"
   end
 
   add_index "issues", ["repository_id"], name: "index_issues_on_repository_id", using: :btree
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20160811175225) do
     t.string   "github_number"
     t.string   "github_id"
     t.string   "assignee"
-    t.string   "started_at"
+    t.datetime "started_at"
   end
 
   add_index "pull_requests", ["repository_id"], name: "index_pull_requests_on_repository_id", using: :btree

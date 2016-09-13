98329fcf624b059fccf1805798ecd6bd4b7a3969
Jorge Elizondo <Jelizondolevy.s@gmail.com>
2016-09-13 15:36:44 -0500

    fixed two failures, one in organizationController and one in repositories controller

--- spec/controllers/admin/organizations_controller_spec.rb
+++ spec/controllers/admin/organizations_controller_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Admin::OrganizationsController, type: :controller do
   describe "GET #index" do
+    before(:each) do
+     @user = FactoryGirl.create :user
+     sign_in @user
+    end
+
     it "renders the :index view" do
       get :index
       expect(response).to render_template :index
     end
   end
 end
-

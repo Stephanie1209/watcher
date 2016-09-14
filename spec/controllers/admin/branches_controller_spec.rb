3b8807852281501f320c118b185fc2b7a7aa6084
tonyyasi <tonyyasi@gmail.com>
2016-09-14 13:22:42 -0500

    Deletes sketchy tests. And fix identation in controllrs

--- spec/controllers/admin/branches_controller_spec.rb
+++ spec/controllers/admin/branches_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-
 RSpec.describe Admin::BranchesController, type: :controller do
   describe "GET #index" do
-     before(:each) do
-    @user = FactoryGirl.create :user
-    sign_in @user
-  end
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+    end
 
     it "renders the :index view" do
       get :index, :repository_id => 'watcher'
--- spec/controllers/admin/commits_controller_spec.rb
+++ spec/controllers/admin/commits_controller_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Admin::CommitsController, type: :controller do
   describe "GET #index" do
-     before(:each) do
- @organization = FactoryGirl.create :icalialabs
-    @repository =  FactoryGirl.create :watcher, organization: @organization
-    @branch = FactoryGirl.create :branch ,name: "master",repository: @repository
-    @user = FactoryGirl.create :user
-    sign_in @user
-  end
+    before(:each) do
+      organization = FactoryGirl.create :icalialabs
+      repository =  FactoryGirl.create :watcher, organization: organization
+      FactoryGirl.create :branch ,name: "master",repository: repository
+      @user = FactoryGirl.create :user
+      sign_in @user
+    end
+
     it "renders the :index view" do
-      get :index, :repository_id => 'watcher', :branch_id => 'master'
+      get :index, repository_id: 'watcher', branch_id: 'master'
       expect(response).to render_template :index
     end
   end

4e7a31568eebbeac055c57dd5599d0843b21c8d0
Jorge Elizondo <Jelizondolevy.s@gmail.com>
2016-10-11 15:57:45 -0500

    changed strings to constants in pull_request model

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -1,6 +1,8 @@
 class PullRequest < ActiveRecord::Base
+  CLOSED = 'closed'
+  OPEN = 'open'
   belongs_to :repository
-  scope :closed, -> { where(status: 'closed') }
-  scope :open, -> { where(status: 'open') }
+  scope :closed, -> { where(status: 'CLOSED') }
+  scope :open, -> { where(status: 'OPEN') }
   scope :order_by_newest, -> { order(started_at: :desc) }
 end

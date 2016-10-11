e904b1e5b767e8a6a7df7d4a74b9ee6e82acb7d4
Jorge Elizondo <Jelizondolevy.s@gmail.com>
2016-10-11 16:01:45 -0500

    fixed error in changes of constants

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -2,7 +2,7 @@ class PullRequest < ActiveRecord::Base
   CLOSED = 'closed'
   OPEN = 'open'
   belongs_to :repository
-  scope :closed, -> { where(status: 'CLOSED') }
-  scope :open, -> { where(status: 'OPEN') }
+  scope :closed, -> { where(status: CLOSED) }
+  scope :open, -> { where(status: OPEN) }
   scope :order_by_newest, -> { order(started_at: :desc) }
 end
